c34ec2dab6280f1b72d49241c3461f2d8612d9b7
manojkhandve <manojkhandve707@gmail.com>
2024-08-16 21:24:06 +0530

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,84 @@
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram,  BsYoutube, BsWhatsapp } from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
+              to='/'
+              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+            >
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                Daily
+              </span>
+              Blog's
+            </Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href=''
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Portfolio
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Daily Blog's
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.github.com/sahandghavidel'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                 Whatsapp
+                </Footer.Link>
+                <Footer.Link href='#'>Telegram</Footer.Link>
+    
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='/privacy'>Privacy Policy</Footer.Link>
+                <Footer.Link href='/disclaimer'>Disclaimer</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="Daily Blog's"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsInstagram}/>
+            <Footer.Icon href='#' icon={BsYoutube}/>
+            <Footer.Icon href='#' icon={BsWhatsapp}/>
+
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
+
+
+
